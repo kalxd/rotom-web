c8a563053e7950d077eedc337d1a1007537c5559
荀洪道 <kalxd@126.com>
2025-07-10 15:01:09 +0800

    登录逻辑。

--- src/page/login.ts
+++ src/page/login.ts
@@ -1,8 +1,10 @@
 import * as m from "drifloon/m";
+import { EitherAsync } from "drifloon/purify";
 import { Form, FormAttr, RequireField, TrimInput } from "drifloon/form";
 import { PrimaryButton, Segment, SegmentAttr, SegmentStyle, Header } from "drifloon/element";
-import { formMut } from "drifloon/data";
+import { ValidatorResult, formMut, isNotEmpty, must } from "drifloon/data";
 import { Align, Color } from "drifloon/data/var";
+import { SessionUserC, sessionUserC } from "../ty";
 
 interface FormField {
 	username: string,
@@ -14,18 +16,50 @@ const makeFormField = (username: string, password: string): FormField => ({
 	password
 });
 
+const loginApi = (data: FormField): EitherAsync<Error, SessionUserC> => {
+	return EitherAsync(async helper => {
+		const rsp = await fetch(
+			"/api/user/login",
+			{
+				method: "POST",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			.then(r => r.json());
+
+		const sessionUser = sessionUserC
+			.decode(rsp)
+			.mapLeft(msg => new Error(msg));
+
+		return helper.liftEither(sessionUser);
+	});
+};
+
+const validateForm = (data: FormField): ValidatorResult<SessionUserC> => {
+	const formData = must("用户名", isNotEmpty(data.username))
+		.must("密码", isNotEmpty(data.password))
+		.collect(makeFormField);
+
+	return EitherAsync.liftEither(formData)
+		.chain(data => {
+			return loginApi(data).mapLeft(e => [`请求出错：${e.message}`]);
+		});
+};
+
 export interface LoginAttr {
 	connectLogin: () => void;
 }
 
-const Main = (): m.Component => {
+const Main: m.ComponentTypes<LoginAttr> = () => {
 	const fd = formMut<FormField>({
 		username: "",
 		password: ""
 	});
 
 	return {
-		view: () => {
+		view: ({ attrs }) => {
 			const formAttr: FormAttr<FormField> = {
 				formdata: fd
 			};
@@ -33,6 +67,11 @@ const Main = (): m.Component => {
 			const segmentAttr: SegmentAttr = {
 				color: Color.Teal,
 				style: SegmentStyle.Stack
+			};
+
+			const onsubmit = async (): Promise<void> => {
+				const rsp = await fd.validate(validateForm);
+				rsp.ifRight(attrs.connectLogin)
 			};
 
 			return m.fragment({}, [
@@ -48,7 +87,7 @@ const Main = (): m.Component => {
 							m("label", "密码"),
 							m(TrimInput, { bindValue: fd.prop("password") })
 						]),
-						m(PrimaryButton, "提交")
+						m(PrimaryButton, { connectClick: onsubmit }, "提交")
 					])
 				])
 			]);
--- src/ty.ts
+++ src/ty.ts
@@ -6,3 +6,10 @@ export const userC = C.Codec.interface({
 });
 
 export type UserC = C.GetType<typeof userC>;
+
+export const sessionUserC = C.Codec.interface({
+	token: C.string,
+	user: userC
+});
+
+export type SessionUserC = C.GetType<typeof sessionUserC>;

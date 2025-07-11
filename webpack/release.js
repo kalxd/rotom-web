128d8afc8508ff6c3a394b9f7ae4100a2b4e7b51
荀洪道 <kalxd@126.com>
2025-07-11 19:46:28 +0800

    .

---
+++ webpack/release.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const config = {
+	mode: "production",
+	entry: "./src/main.ts",
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader"
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+	output: {
+		path: path.resolve("./assert/")
+	}
+};
+
+module.exports = config;

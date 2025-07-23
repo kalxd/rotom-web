25db88f4f2b74974fc6f63bcac2152d81bb47ec3
荀洪道 <kalxd@126.com>
2025-07-23 18:42:56 +0800

    更完整的http客户端。

--- src/app/data/http.ts
+++ src/app/data/http.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as R from "rxjs";
+import { z } from "zod";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class Http {
+	private readonly http = inject(HttpClient);
+
+	makeGet<R>(url: string, codec: z.ZodType<R>): Observable<R> {
+		return this.http.get(url).pipe(R.map(x => codec.parse(x)))
+	}
+
+	makePost<T, R>(url: string, body: T | null, codec: z.ZodType<R>): Observable<R> {
+		return this.http.post(url, body).pipe(R.map(x => codec.parse(x)));
+	}
 }

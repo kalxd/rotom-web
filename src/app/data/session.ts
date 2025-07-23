5bc8248a134acbda7b94744d1095ca3ae785e98e
荀洪道 <kalxd@126.com>
2025-07-23 16:25:06 +0800

    添加基础文件。

---
+++ src/app/data/session.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { z } from "zod";
+
+const TOKEN_KEY = "token";
+
+export const readToken = (): string | null => {
+	return localStorage.getItem(TOKEN_KEY);
+};
+
+export const writeToken = (token: string): void => {
+	localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const userZ = z.object({
+	id: z.number(),
+	username: z.string()
+});
+
+export type UserZ = z.infer<typeof userZ>;
+
+export const sessionZ = z.object({
+	token: z.string(),
+	user: userZ
+});
+
+export type SessionZ = z.infer<typeof sessionZ>;
+
+@Injectable({
+	providedIn: 'root'
+})
+export class Session {
+	session: SessionZ | undefined;
+}

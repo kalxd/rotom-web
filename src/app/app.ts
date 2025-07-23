b7bba3f64fd540066b9498dadcaa94322350363f
荀洪道 <kalxd@126.com>
2025-07-23 15:08:45 +0800

    基本操练。

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButton } from "@angular/material/button";
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [
+		RouterOutlet,
+		MatButton
+	],
 	templateUrl: './app.html',
 	styleUrl: './app.css'
 })
 export class App {
 	protected readonly title = signal('rotom-web');
+
+	buttonClickE(): void {
+		alert("do this?");
+		console.log("do this?");
+	}
 }

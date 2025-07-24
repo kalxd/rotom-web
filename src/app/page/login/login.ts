d4d9bfdd1c918812493caa485ae4423075004109
荀洪道 <kalxd@126.com>
2025-07-24 14:18:19 +0800

    添加登录界面。

--- src/app/page/login/login.ts
+++ src/app/page/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'xg-login',
-	imports: [],
+	imports: [
+		ReactiveFormsModule,
+
+		MatToolbarModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule
+	],
 	templateUrl: './login.html',
 	styleUrl: './login.css'
 })
 export class Login {
+	private formBuilder = inject(FormBuilder);
 
+	loginForm = this.formBuilder.group({
+		username: ["", Validators.required],
+		password: ["", Validators.required]
+	});
+
+	beginSubmitLoginForm(): void {
+		console.log(this.loginForm.value);
+	}
 }

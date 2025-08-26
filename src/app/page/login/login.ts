06862d078ef6c7a9e6aaedf92fba9d7da401c05c
荀洪道 <kalxd@126.com>
2025-08-26 12:13:48 +0800

    完全移除material组件。

--- src/app/page/login/login.ts
+++ src/app/page/login/login.ts
@@ -1,10 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatCardModule } from "@angular/material/card";
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { LoginApi } from './api';
 import { Session } from '../../data/session';
 import { UiContainer, UiForm, UiFormField } from 'drifloon';
@@ -15,13 +10,7 @@ import { UiContainer, UiForm, UiFormField } from 'drifloon';
 		ReactiveFormsModule,
 		UiContainer,
 		UiForm,
-		UiFormField,
-
-		MatToolbarModule,
-		MatCardModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatButtonModule
+		UiFormField
 	],
 	templateUrl: './login.html',
 	styleUrl: './login.scss'
